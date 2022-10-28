f5e3d29729208e5b9c31f6ba4d9cdac5755a4216
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2022-10-28 21:00:30 +0530

    stack-updated

--- data_structures/Stack.py
+++ data_structures/Stack.py
@@ -1,22 +1,25 @@
+from collections import deque
+
+
 class Stack:
     def __init__(self):
-        self.items = []
+        self.container = deque()
 
     def is_empty(self):
-        return len(self.items) == 0
+        return len(self.container) == 0
 
     def size(self):
-        return len(self.items)
+        return len(self.container)
 
     def push_item(self, item):
-        self.items.append(item)
+        self.container.append(item)
 
     def pop_item(self):
-        print("removing {}.".format(self.peek_item()))
-        return self.items.pop()
+        # print("removing {}.".format(self.peek_item()))
+        return self.container.pop()
 
     def peek_item(self):
-        return self.items[-1]
+        return self.container[-1]
 
     def __str__(self):
-        return "[" + ", ".join(self.items) + "]"
+        return "[" + ", ".join(self.container) + "]"
